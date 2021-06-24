1bbec92f00f2b13f3b52284f9ef387c68febcf41
ahmed <ahmedlearn01@gmail.com>
2021-06-24 12:21:55 -0700

    done json server

--- app.js
+++ app.js
@@ -1,4 +1,41 @@
+const ul = document.querySelector('ul');
+const form = document.querySelector('form');
+
+const htmlTemplete = (data)=>{
+    const time = data.created_at.toDate();
+    const html = `
+        <li>
+            <div class="box">
+                <div>${data.title}</div>
+                <small>${time}</small>
+            </div>
+            <button>Delete</button>
+        </li>
+    `
+    ul.innerHTML += html;
+}
 db.collection('practicing').get().then(snapshot=>{
-    console.log(snapshot.docs[0].data());
+    snapshot.docs.forEach(doc=>{
+        htmlTemplete(doc.data());
+    })
 
 }).catch(err=>console.log(err));
+
+form.addEventListener('submit',e=>{
+    e.preventDefault();
+    const now = new Date();
+    const fruit = {
+        title: form.fruit.value,
+        created_at: firebase.firestore.Timestamp.fromDate(now)
+    }
+
+    db.collection('practicing').add(fruit).then(()=>{
+        console.log('fruit added')
+    }).catch(err=>console.log(err));
+})
+
+ul.addEventListener('click',e=>{
+    if(e.target.tagName === 'BUTTON'){
+        console.log(e.target.parentElement)
+    }
+})
